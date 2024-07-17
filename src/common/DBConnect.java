687f2191a6976a3b5e53fcbcbb3187c2e7f72e98
오유찬 <83404310+ohyuchan123@users.noreply.github.com>
2024-07-18 02:47:51 +0900

    :sparkles: config.properties 를 사용하여 DBConnect 관리

--- src/common/DBConnect.java
+++ src/common/DBConnect.java
@@ -1,19 +1,35 @@
 package common;
 
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class DBConnect {
+
+    private static final String PROP_FILE = "config/config.properties";
+    private static Properties prop;
+
+    static {
+        prop = new Properties();
+        try (FileInputStream fis = new FileInputStream(PROP_FILE)) {
+            prop.load(fis);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
     // Mysql 정보
     static final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
-    static final String DB_URL = "jdbc:mysql://localhost:3306/moviedb";
-    static final String USER = "root"; // MySQL 사용자 이름
-    static final String PASS = ""; // MySQL 비밀번호
+    static final String DB_URL = prop.getProperty("db_url");
+    static final String USER = prop.getProperty("db_user");
+    static final String PASS = prop.getProperty("db_password");
 
-    // 데이터베ㅣ스 연결 객체 반환 메서드
-    public static Connection getConnection() throws SQLException , ClassNotFoundException {
+    // 데이터베이스 연결 객체 반환 메서드
+    public static Connection getConnection() throws SQLException, ClassNotFoundException {
         Class.forName(JDBC_DRIVER);
-        return DriverManager.getConnection(DB_URL,USER,PASS);
+        return DriverManager.getConnection(DB_URL, USER, PASS);
     }
 }
