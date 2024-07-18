419613dae51d1d6054372fcc92e2af65e9b80092
오유찬 <83404310+ohyuchan123@users.noreply.github.com>
2024-07-18 18:46:24 +0900

    :sparkles: 헤더 패널 추가

--- src/GUI/DateReservationScreen.java
+++ src/GUI/DateReservationScreen.java
@@ -1,11 +1,23 @@
 package GUI;
 
 import javax.swing.*;
+import java.awt.*;
+import java.util.Calendar;
 
 public class DateReservationScreen extends JFrame {
+    private JLabel monthLabel;
+    private JPanel calendarPanel;
+    private Calendar cal;
+
     public DateReservationScreen() {
         setTitle("영화별 예매");
         setSize(300, 200);
+
+        //헤더 패널(상단 파란색 부분)
+        JPanel headerPanel = new JPanel();
+        headerPanel.setBackground(new Color(65,105,225));
+        headerPanel.setPreferredSize(new Dimension(300,50));
+        add(headerPanel, BorderLayout.NORTH);
 
         // 프로그램 종료 설정 추가
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // 창을 닫으면 프로그램이 종료됨
